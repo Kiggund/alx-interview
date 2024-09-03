89dcba248d8ff1d921d0df0f8b9aaa4ae0ac6556
kiggund <abdulbasitkiggundu@gmail.com>
2024-09-03 12:47:06 -0700

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,21 +4,36 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
+if (!movieId) {
+  console.error('Please provide a Movie ID');
+  process.exit(1);
+}
+
 request(url, (error, response, body) => {
   if (error) {
-    console.error(error);
-  } else {
-    const film = JSON.parse(body);
-    const characters = film.characters;
-    characters.forEach(character => {
-      request(character, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        }
-      });
-    });
+    console.error('Error:', error);
+    return;
   }
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch data. Status code:', response.statusCode);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+  characters.forEach(character => {
+    request(character, (error, response, body) => {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+      if (response.statusCode !== 200) {
+        console.error('Failed to fetch character data. Status code:', response.statusCode);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
 });
