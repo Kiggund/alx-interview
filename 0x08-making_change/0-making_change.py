ed603f6c45595545a66ee724a97978904db0b050
kiggund <abdulbasitkiggundu@gmail.com>
2024-11-30 19:00:52 +0000

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,21 +11,19 @@ def makeChange(coins, total):
         If total is 0 or less, return 0
         If total cannot be met by any number of coins you have, return -1
     """
-    if not coins or coins is None:
-        return -1
     if total <= 0:
         return 0
-    change = 0
-    coins = sorted(coins)[::-1]
+    if not coins:
+        return -1
+
+    """ Initialize the dp array with a value greater
+        than the possible number of coins
+    """
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
     for coin in coins:
-        while coin <= total:
-            total -= coin
-            change += 1
-            print(
-                    f"Using coin: {coin},"
-                    f"remaining total: {total},"
-                    f"coins used: {change}"
-            )
-        if total == 0:
-            return change
-    return -1
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
