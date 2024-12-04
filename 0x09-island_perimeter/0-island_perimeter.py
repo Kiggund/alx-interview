651673c97c39808a54a963f46bc773b9b3b4f499
Kiggundu abdulbasit <105321318+Kiggund@users.noreply.github.com>
2024-12-04 18:06:57 +0300

    Corrected pep8 styling

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,6 +5,7 @@ This module contains the function to calculate the perimeter of an island
 described in a grid.
 """
 
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of an island in a grid.
