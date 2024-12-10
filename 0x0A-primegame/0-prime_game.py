1f9015d257df53f5a6f981e8bba2703d30bdc6cf
Kiggundu abdulbasit <105321318+Kiggund@users.noreply.github.com>
2024-12-10 10:01:02 +0300

    Update 0-prime_game.py, updated output

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,50 +1,42 @@
-#!/usr/bin/python3
+/usr/bin/python3
+"""
+Prime game module
+"""
 
 
-def isWinner(x, nums):
-    def sieve(n):
-
-        """ Helper function to generate a list of primes up to n
-        using the Sieve of Eratosthenes """
-
-        is_prime = [True] * (n + 1)
-        p = 2
-        while (p * p <= n):
-            if is_prime[p]:
+def is_winner(x, nums):
+    """
+    a function to determine the winner of the prime game.
+    """
+    def primes_up_to(n):
+        """
+        Generate all prime numbers up to n using the Sieve of Eratosthenes.
+        """
+        if n < 2:
+            return []
+        sieve = [True] * (n + 1)
+        sieve[0], sieve[1] = False, False
+        for p in range(2, int(n**0.5) + 1):
+            if sieve[p]:
                 for i in range(p * p, n + 1, p):
-                    is_prime[i] = False
-            p += 1
-        return [p for p in range(2, n + 1) if is_prime[p]]
-
-    def play_game(n):
-        """ Simulate the game for a given n """
-        primes = sieve(n)
+                    sieve[i] = False
+        return [p for p in range(2, n + 1) if sieve[p]]
+    maria_wins = 0
+    ben_wins = 0
+    for n in nums:
+        primes = primes_up_to(n)
         moves = 0
         while primes:
             prime = primes.pop(0)
             primes = [p for p in primes if p % prime != 0]
             moves += 1
-            """
-            Maria wins if moves are odd,
-             Benwinsif moves are even
-            """
-        return moves % 2 == 1
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if play_game(n):
-            maria_wins += 1
-        else:
+        if moves % 2 == 0:
             ben_wins += 1
-
+        else:
+            maria_wins += 1
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
-
-
-print("Winner:", isWinner(5, [2, 5, 1, 4, 3]))
